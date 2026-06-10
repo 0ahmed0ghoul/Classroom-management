400e2232fccfb271f0b05ace1546324d8be2d398
Ahmed <0ahmedghoul0@gmail.com>
2026-06-10 20:38:44 +0100

    fixes

--- F/src/App.jsx
+++ F/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Admin from "./Admin";
+import AdminPage from "./AdminPage";
 import StudentAbs from "./absences/StudentAbs";
 import AllStudents from "./Modals/Allstudents";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Admin />} />
+      <Route path="/" element={<AdminPage />} />
       <Route path="/attend/allstudents" element={<AllStudents />} />
       <Route path="/admin/student/:id/absences" element={<StudentAbs />} />
       <Route path="/admin/module/:id/types" element={<StudentAbs />} />
